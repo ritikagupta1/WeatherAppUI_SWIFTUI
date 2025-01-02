4483306e0bc685659b6cfc183ff5969a0889ff3b
Ritika Gupta <ritikagupta2020@gmail.com>
2025-01-02 15:47:29 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Ritika Gupta on 02/01/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
